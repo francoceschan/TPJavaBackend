9187da2a78de1542ef7c1264538b640d27c8dd3e
Franco Ceschan <francoceschan@gmail.com>
2024-05-20 20:02:10 -0300

    Creo el proyecto

---
+++ src/main/java/francoceschan/TPJavaBackend/TpJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package francoceschan.TPJavaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpJavaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/francoceschan/TPJavaBackend/TpJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package francoceschan.TPJavaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
