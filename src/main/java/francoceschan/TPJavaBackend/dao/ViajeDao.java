7acfac116430999f8cf556647643e917f95a39eb
Franco Ceschan <francoceschan@gmail.com>
2024-11-10 14:58:48 -0300

    cambio fecha_hora por fecha_hora_inicio en query

--- src/main/java/francoceschan/TPJavaBackend/dao/ViajeDao.java
+++ src/main/java/francoceschan/TPJavaBackend/dao/ViajeDao.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface ViajeDao extends JpaRepository<Viaje, Long>, JpaSpecificationExecutor<Viaje> {
 
-    @Query(value = "SELECT * FROM viajes WHERE fecha_hora > NOW()", nativeQuery = true)
+    @Query(value = "SELECT * FROM viajes WHERE fecha_hora_inicio > NOW()", nativeQuery = true)
     List<Viaje> getViajesDisponibles();
 
     long countViajeByColectivo_Patente(String patente);
